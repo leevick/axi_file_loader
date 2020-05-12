64f3ed75f1ee567ebfacda2ad05bf70c2f18aaaf
leevick <leevick@tsinghua.edu.cn>
2020-05-12 10:56:58 +0800

    [FIX]Writting timing problem

---
+++ axi_file_loader.srcs/sources_1/bd/testbench/hdl/testbench_wrapper.v
@@ -0,0 +1,18 @@
+//Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
+//--------------------------------------------------------------------------------
+//Tool Version: Vivado v.2019.1 (lin64) Build 2552052 Fri May 24 14:47:09 MDT 2019
+//Date        : Tue May 12 10:23:54 2020
+//Host        : DELL running 64-bit Ubuntu 18.04.4 LTS
+//Command     : generate_target testbench_wrapper.bd
+//Design      : testbench_wrapper
+//Purpose     : IP block netlist
+//--------------------------------------------------------------------------------
+`timescale 1 ps / 1 ps
+
+module testbench_wrapper
+   ();
+
+
+  testbench testbench_i
+       ();
+endmodule
--- axi_file_loader.srcs/sources_1/new/axi_file_loader.v
+++ axi_file_loader.srcs/sources_1/new/axi_file_loader.v
@@ -25,13 +25,15 @@
 
 module axi_file_loader #
 (
-    parameter filename = "rule.bin"
+    parameter filename = "rule.bin",
+    parameter address_width = 16,
+    parameter data_width = 32
 )
 (
         input m_axi_aclk,
         input m_axi_aresetn,
 
-        output [15:0] m_axi_awaddr,
+        output [address_width-1:0] m_axi_awaddr,
         output [7:0] m_axi_awlen,
         output [2:0] m_axi_awsize,
         output [1:0] m_axi_awburst,
@@ -40,15 +42,15 @@ module axi_file_loader #
         output [2:0] m_axi_awprot,
         output m_axi_awvalid,
         input m_axi_awready,
-        output [63:0] m_axi_wdata,
-        output [7:0] m_axi_wstrb,
+        output [data_width*8-1:0] m_axi_wdata,
+        output [data_width-1:0] m_axi_wstrb,
         output m_axi_wlast,
         output m_axi_wvalid,
         input m_axi_wready,
         input [1:0] m_axi_bresp,
         input m_axi_bvalid,
         output m_axi_bready,
-        output [15:0] m_axi_araddr,
+        output [address_width-1:0] m_axi_araddr,
         output [7:0] m_axi_arlen,
         output [2:0] m_axi_arsize,
         output [1:0] m_axi_arburst,
@@ -57,16 +59,16 @@ module axi_file_loader #
         output [2:0] m_axi_arprot,
         output m_axi_arvalid,
         input m_axi_arready,
-        input [63:0] m_axi_rdata,
+        input [data_width*8-1:0] m_axi_rdata,
         input [1:0] m_axi_rresp,
         input m_axi_rlast,
         input m_axi_rvalid,
         output m_axi_rready,
 
-        output cfg_prepared
+        output reg cfg_prepared
 );
 
-    reg [15:0] m_axi_awaddr_buf = 0;
+    reg [address_width-1:0] m_axi_awaddr_buf = 0;
     reg [7:0] m_axi_awlen_buf = 0;
     reg [2:0] m_axi_awsize_buf = 0;
     reg [1:0] m_axi_awburst_buf = 0;
@@ -74,12 +76,12 @@ module axi_file_loader #
     reg [3:0] m_axi_awcache_buf = 0;
     reg [2:0] m_axi_awprot_buf = 0;
     reg m_axi_awvalid_buf = 0;
-    reg [63:0] m_axi_wdata_buf = 0;
-    reg [7:0] m_axi_wstrb_buf = 0;
+    reg [data_width*8-1:0] m_axi_wdata_buf = 0;
+    reg [data_width-1:0] m_axi_wstrb_buf = 0;
     reg m_axi_wlast_buf = 0;
     reg m_axi_wvalid_buf = 0;
     reg m_axi_bready_buf = 1;
-    reg [15:0] m_axi_araddr_buf = 0;
+    reg [address_width-1:0] m_axi_araddr_buf = 0;
     reg [7:0] m_axi_arlen_buf = 0;
     reg [2:0] m_axi_arsize_buf = 0;
     reg [1:0] m_axi_arburst_buf = 0;
@@ -87,7 +89,7 @@ module axi_file_loader #
     reg [3:0] m_axi_arcache_buf = 0;
     reg [2:0] m_axi_arprot_buf = 0;
     reg m_axi_arvalid_buf = 0;
-    reg m_axi_rready_buf = 0;
+    reg m_axi_rready_buf = 1;
     reg cfg_prepared_buf = 0;
 
     assign m_axi_awaddr = m_axi_awaddr_buf;
@@ -112,61 +114,53 @@ module axi_file_loader #
     assign m_axi_arprot = m_axi_arprot_buf;
     assign m_axi_arvalid = m_axi_arvalid_buf;
     assign m_axi_rready = m_axi_rready_buf;
-    assign cfg_prepared = cfg_prepared_buf;
 
-integer data_file,rv,i=0,j;
-reg [63:0] buffer;
+    integer data_file,rv,i=0,j,k,enb = 1;
+    reg [7:0] buffer;
+    reg [address_width-1:0] address = 0;
 
 initial begin
+    cfg_prepared = 0;
+    m_axi_awsize_buf = $clog2(data_width);
     data_file = $fopen(filename,"rb");
     if (!data_file) begin
         $display("data_file handle was NULL");
     end else begin
         $display("normal!");
     end
-    @(posedge m_axi_wready);
-    m_axi_awvalid_buf = 1;
-    m_axi_awsize_buf = 3;
-    m_axi_awcache_buf = 3;
-    while (!$feof(data_file)) begin
-        rv = $fread(buffer,data_file);
-        //write address
-        m_axi_awaddr_buf = 8*i;
-        i = i + 1;
+    @(posedge m_axi_aresetn);
+    #100 ;
+    @(posedge m_axi_aclk);
+    while (enb) begin
         @(posedge m_axi_aclk);
-        @(posedge m_axi_aclk);
+        m_axi_awvalid_buf = 1;
         m_axi_wvalid_buf = 1;
-        m_axi_wdata_buf = buffer;
-        m_axi_wstrb_buf = 8'hff;
         m_axi_wlast_buf = 1;
+        m_axi_awaddr_buf = address;
+        for (i = 0;i < data_width;i = i+1) begin
+            rv = $fread(buffer,data_file);
+            if (rv < 1)
+                enb = 0;
+
+            if(enb!=0) begin
+                m_axi_wstrb_buf[i] = 1;
+                m_axi_wdata_buf[8*i+7-:8] = buffer;
+            end else begin
+                m_axi_wstrb_buf[i] = 0;
+                m_axi_wdata_buf[8*i+7-:8] = 0;
+            end
+        end
         @(posedge m_axi_aclk);
+        address = address + data_width;
+        m_axi_awvalid_buf = 0;
         m_axi_wvalid_buf = 0;
         m_axi_wlast_buf = 0;
-        @(posedge m_axi_aclk);
-        @(posedge m_axi_aclk);
+        @(posedge m_axi_bvalid);
         @(posedge m_axi_aclk);
     end
-    m_axi_awsize_buf = 0;
-    m_axi_awvalid_buf = 0;
-    m_axi_awcache_buf = 0;
-    cfg_prepared_buf = 1;
-    cfg_prepared_buf = #1 0;
+    @(posedge m_axi_aclk);
+    cfg_prepared = 1;
     $fclose(data_file);
 end
 
-// always @(posedge clk) begin
-//     if(reset) begin
-//         test_counter = 0;
-//     end else begin
-//         scan_file = $fscanf(data_file, "%d\n", captured_data);
-//     if (!$feof(data_file)) begin
-//     //use captured_data as you would any other wire or reg value;
-//         test[test_counter] = captured_data;
-//         test_counter = test_counter + 1;
-//     end
-// end
-
-
-
-    
 endmodule
